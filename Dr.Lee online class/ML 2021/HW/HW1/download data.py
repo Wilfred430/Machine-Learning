c548b4925ef597f6e2584b205bc669686abe816b
WilfredEE <wendeyang2@gmail.com>
2025-07-26 20:04:31 +0800

    add Step

---
+++ Dr.Lee online class/ML 2021/HW/HW1/download data.py
@@ -0,0 +1,4 @@
+import subprocess
+
+subprocess.run(["gdown", "--id", "19CCyCgJrUxtvgZF53vnctJiOJ23T5mqF", "--output", "covid.train.csv"])
+subprocess.run(["gdown", "--id", "1CE240jLm2npU-tdz81-oVKEF3T2yfT1O", "--output", "covid.test.csv"])
